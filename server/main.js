05fcbcd22749079b92b57013e5c508a59eb91873
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2016-12-08 18:53:05 +1400

    Added the FCM sender to the mhews server, so that the IBL can just publish a warning to the mhews server which triggers sending FCM notification.

--- server/main.js
+++ server/main.js
@@ -4,6 +4,8 @@ import { check } from 'meteor/check'
 import {Warnings} from '../imports/api/warnings.js';
 import {WeatherForecasts} from '../imports/api/weather.js';
 
+var request = require('request');
+
 Meteor.startup(() => {
 
   Meteor.methods({
@@ -34,6 +36,10 @@ function publishWarning(warning){
     check(warning.depth, Number);
   }
 
+  Meteor.defer(function(){    
+    sendFcmNotification(warning);
+  })
+
   return Warnings.insert(warning);
 }
 
@@ -50,4 +56,65 @@ function publishWeatherForecast(forecast){
   }]);
 
   return WeatherForecasts.insert(forecast);
+}
+
+function sendFcmNotification(warning){
+  const fcmApiKey = Meteor.settings.fcmApiKey;
+
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "key="+fcmApiKey
+  }
+  const options = {
+    url: "https://fcm.googleapis.com/fcm/send",
+    method: "POST",
+    headers: headers,
+    json: getWarningMessageForFcm(warning)
+  }
+  request.post(options, function(error, response){
+    if (!error && response.statusCode == 200) {
+      console.log("FCM notification was sent for a warning successfully.");
+    } else {
+      console.log('error: '+ response.statusCode+ " "+response.statusMessage);
+    }
+  });
+}
+
+function getWarningMessageForFcm(warning){
+  const fcmMessage = {
+    "to": "/topics/disaster",
+    "priority": "high",
+    "notification" : {
+      "body" : warning.type + " " + warning.level,
+      "title" : "Disaster",
+      "click_action" : "FCM_PLUGIN_ACTIVITY",
+      "sound": "default",
+      "icon" : "myicon"
+    },
+    "data" : {
+      "type": warning.type,
+      "level": warning.level
+    }
+  };
+
+  if( warning.type == "tsunami" || warning.type == "earthquake" ){
+    const mw = warning.mw;
+    fcmMessage.notification.body = "Earthquake (Magnitude "+mw+")";
+    fcmMessage.data.epicenter_lat = warning.epicenter.lat;
+    fcmMessage.data.epicenter_lng = warning.epicenter.lng;
+    fcmMessage.data.mw = mw;
+    fcmMessage.data.depth = warning.epicenter.depth;
+  }
+  else if( warning.type == "heavyRain" ){
+    fcmMessage.data.area = warning.area;
+    fcmMessage.data.direction = warning.direction;
+  }
+  else if( warning.type == "cyclone" ){
+    fcmMessage.data.name = warning.name; // Name of the TC
+    fcmMessage.data.category = warning.category;
+  }
+
+  console.log(JSON.stringify(fcmMessage));
+
+  return fcmMessage;
 }
