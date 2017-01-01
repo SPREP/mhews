99968442680e26264babdb29b44bf5312c055e02
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-01-02 13:50:18 +1400

    Removed AboutSMD page. Removed unnecessary items from settings.json and componentHelper.js

--- imports/api/client/componentHelper.js
+++ imports/api/client/componentHelper.js
@@ -2,15 +2,11 @@
    Disable the eslint rule to check unused vars in this file.
 */
 /* eslint no-unused-vars: 0 */
-import WarningListContainer from '../../ui/WarningList.jsx';
-import WeatherPageContainer from '../../ui/Weather.jsx';
-import CyclonePage from '../../ui/Cyclone.jsx';
-import EarthquakePage from '../../ui/Earthquake.jsx';
-import HeavyRainPage from '../../ui/HeavyRain.jsx';
-import AboutSMDPage from '../../ui/AboutSMD.jsx';
 import AboutAppPage from '../../ui/AboutAppPage.jsx';
 import PreferencesPageContainer from '../../ui/PreferencesPage.jsx';
 import TopPage from "../../ui/TopPage.jsx";
+
+// Icons
 import HomeIcon from 'material-ui/svg-icons/action/home';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
 import WarningIcon from 'material-ui/svg-icons/alert/warning';
