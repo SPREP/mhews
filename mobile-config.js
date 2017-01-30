efb22f12827eac5be58854b867481ccdf070aaa5
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-01-30 23:07:23 +1400

    Version 0.1.6

--- mobile-config.js
+++ mobile-config.js
@@ -1,6 +1,6 @@
 App.info({
   id: 'ws.gov.samet.mhews',
-  version: "0.1.5",
+  version: "0.1.6",
   name: 'Samoa weather',
   description: 'Multi-Hazard Early Warning System mobile app',
   author: 'Samoa Meteorology Division',
