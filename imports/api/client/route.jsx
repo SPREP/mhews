c86580d73063ce7604401c247f25935e14907ae6
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-01-31 17:40:02 +1400

    Removed admin pages

--- imports/api/client/route.jsx
+++ imports/api/client/route.jsx
@@ -17,10 +17,6 @@ import App from '../../ui/App.jsx';
 import AppTemplate from '../../ui/AppTemplate.jsx';
 import InitPage from '../../ui/InitPage.jsx';
 
-import Admin from '../../ui/admin/Admin.jsx';
-import AdminDashboard from '../../ui/admin/AdminDashboard.jsx';
-import WeatherDayIconMatrix from '../../ui/admin/WeatherDayIconMatrix.jsx';
-
 export function initRouter(){
 
   render(
@@ -37,32 +33,6 @@ export function initRouter(){
         <Route path="cyclone/:id" component={CyclonePage} />
         <IndexRoute component={TopPage} />
       </Route>
-      <IndexRedirect to="/app" />
-    </Route>
-  </Router>
-  , document.getElementById('render-target'));
-}
-
-export function initRouterForBrowser(){
-
-  render(
-    <Router history={ browserHistory }>
-      <Route path="/" component={AppTemplate}>
-      <Route path="init" component={InitPage} />
-      <Route path="app" component={App} >
-        <Route path="about" component={AboutAppPage} />
-        <Route path="usage" component={UsagePage} />
-        <Route path="settings" component={PreferencesPage} />
-        <Route path="earthquake/:id" component={EarthquakePage} />
-        <Route path="tsunami/:id" component={EarthquakePage} />
-        <Route path="heavyRain/:id" component={HeavyRainPage} />
-        <Route path="cyclone/:id" component={CyclonePage} />
-        <IndexRoute component={TopPage} />
-      </Route>
-      <Route path="/admin" component={Admin} >
-        <Route path="day-weather-matrix/:id" component={WeatherDayIconMatrix}/>
-        <IndexRoute component={AdminDashboard} />
-      </Route>e
       <IndexRedirect to="/app" />
     </Route>
   </Router>
