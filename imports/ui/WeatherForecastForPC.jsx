93d613257098b6b121fc4f33dc0fa5d5044992e6
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-08-04 17:01:29 +1300

    Display the forecast of whole week in the PC browser app.

--- imports/ui/WeatherForecastForPC.jsx
+++ imports/ui/WeatherForecastForPC.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import Card from 'material-ui/Card/Card';
+import CardHeader from 'material-ui/Card/CardHeader';
 import {Nowcast} from './components/Nowcast.jsx';
+import {WeatherSituation} from './WeatherSituationForMobile.jsx';
+
+const cardStyle = {display: "inline-block", width: 300, verticalAlign: "top"};
 
 /*
   Similar to the WeatherPage, but the forecast for all the days in the extended forecast period
@@ -10,23 +14,39 @@ import {Nowcast} from './components/Nowcast.jsx';
 export class WeatherForecastForPC extends React.Component {
 
   render(){
+    const t = this.props.t;
+
     return (
-      <div>
+      <Card>
         {
           this.props.forecasts.map((forecast, index) => {
             return (
-              <Card key={index}>
+//              <Card key={index} style={cardStyle}>
+<div style={cardStyle}>
                 <Nowcast
                   forecast={forecast}
                   t={this.props.t}
                 />
-              </Card>
+              </div>
+//              </Card>
             )
           })
         }
-      </div>
+        <CardHeader
+          title={t("Weather")+" "+t("situation")}
+          showExpandableButton={false}
+          subtitle={t("Issued_at")+" "+this.dateTimeToString(this.props.issuedAt)}
+        />
+        <WeatherSituation t={t} expandable={false} situation={this.props.situation} />
+
+</Card>
     );
   }
+
+
+    dateTimeToString(dateTime){
+      return moment(dateTime).format("YYYY-MM-DD HH:mm");
+    }
 }
 
 WeatherForecastForPC.propTypes = {
