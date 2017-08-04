379ac7e4a3bd7310ce38d563c3088c915a7b5b8b
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-08-04 21:10:34 +1300

    Added clean up of destination folder before release build.

--- private/deploy_scripts/release_mobile.sh
+++ private/deploy_scripts/release_mobile.sh
@@ -5,6 +5,11 @@ export APPNAME=mhews
 export KEYNAME=mhews
 export BUILD_TOOLS_VER=25.0.0
 
+rm -rf $BUILD_OUTPUT_DIR/android
+rm -rf $BUILD_OUTPUT_DIR/ios
+mkdir -p $BUILD_OUTPUT_DIR/android
+mkdir -p $BUILD_OUTPUT_DIR/ios
+
 meteor build $BUILD_OUTPUT_DIR --server=http://mhews.samet.gov.ws:3000 --mobile-settings=settings_release.json
 cd $BUILD_OUTPUT_DIR/android/
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk $KEYNAME
