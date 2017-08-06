e704a3907889ce6f2de6392bdd94b17e1e53ed20
Takeshi Matsumura <takeshi.matsumura@ericsson.com>
2017-08-06 23:53:27 +1300

    Slick is not used for weather situation any more.

--- imports/ui/WeatherSituationForMobile.jsx
+++ imports/ui/WeatherSituationForMobile.jsx
@@ -3,34 +3,22 @@ import Card from 'material-ui/Card/Card';
 import CardHeader from 'material-ui/Card/CardHeader';
 import CardMedia from 'material-ui/Card/CardMedia';
 import CardText from 'material-ui/Card/CardText';
-import Slider from 'react-slick';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
-import RightArrowIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
-import LeftArrowIcon from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
 
 import Config from '/imports/config.js';
-
-class LeftNavButton extends React.Component {
-  render(){
-    return <LeftArrowIcon {...this.props}/>
-  }
-}
-
-class RightNavButton extends React.Component {
-  render(){
-    return <RightArrowIcon {...this.props}/>
-  }
-}
 
 export class WeatherSituation extends React.Component {
 
   constructor(props){
     super(props);
-    this.imageUrls = [
-      Config.cacheFiles.surfaceChart,
-      Config.cacheFiles.satelliteImage
+    this.images = [
+      {
+        title: "Surface Stream Chart",
+        url: Config.cacheFiles.surfaceChart
+      },
+      {
+        title: "Satellite Image",
+        url: Config.cacheFiles.satelliteImage
+      }
     ];
   }
 
@@ -41,53 +29,34 @@ export class WeatherSituation extends React.Component {
   render(){
     const t = this.props.t;
 
-    const settings = {
-      dots: true,
-      infinite: true,
-      arrow: true,
-      speed: 500,
-      slideToShow: 1,
-      slidesToScroll: 1,
-      touchMove: true,
-      touchThreshold: 10,
-      nextArrow: <RightNavButton />,
-      prevArrow: <LeftNavButton />
-    };
-
-    const slides = this.imageUrls;
-
-    if( slides.length == 0 ){
-      return <div></div>;
-    }
-
     return (
-      <Card>
-        <CardHeader
-          title={t("Weather")+" "+t("situation")}
-          showExpandableButton={true}
-          subtitle={t("Issued_at")+" "+this.dateTimeToString(this.props.issuedAt)}
-        />
-        <CardText>
-          {this.props.situation}
-        </CardText>
+      <div>
+        <Card>
+          <CardHeader
+            title={t("Weather")+" "+t("situation")}
+            subtitle={t("Issued_at")+" "+this.dateTimeToString(this.props.issuedAt)}
+          />
+          <CardText>
+            {this.props.situation}
+          </CardText>
+        </Card>
 
-        <CardMedia expandable={true}>
-          <div style={{position: "relative", paddingLeft: "5%", paddingRight: "5%", minWidth: "90%", width: "90%"}}>
-            <Slider {...settings}>
-              {
-                slides.map((slide, key)=>{
-                  return (
-                    <div key={key} >
-                      <img src={slide} style={{width: "100%"}}/>
-                    </div>
-                  );
-                })
-              }
-            </Slider>
-          </div>
-        </CardMedia>
-
-      </Card>
+        {
+          this.images.map(({title, url})=>{
+            return (
+              <Card>
+                <CardHeader
+                  title={title}
+                  showExpandableButton={true}
+                />
+                <CardMedia expandable={true}>
+                  <img src={url} style={{width: "100%"}}/>
+                </CardMedia>
+              </Card>
+            )
+          })
+        }
+      </div>
 
       );
     }
